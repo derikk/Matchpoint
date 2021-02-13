06c252b20919cb65e4748d58b28690fbebd6721b
Derik K <derikk@me.com>
2021-02-13 18:48:25 -0500

    Add Users and Profiles controllers

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,5 +3,6 @@ class Profile < ApplicationRecord
   has_one_attached :photo
 
   enum gender: %i[female male nonbinary]
-  enum sexuality: %i[nobody women men women_and_men nonbinarys women_and_nonbinarys men_and_nonbinarys everybody]
+  enum sexuality: %i[nobody women men women_and_men nonbinarys,
+       women_and_nonbinarys men_and_nonbinarys everybody], _prefix: :loves
 end
