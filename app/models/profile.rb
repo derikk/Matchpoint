9334af7a013c6cbf8073d2bedf8fa311fda7294e
Derik K <derikk@me.com>
2021-03-31 01:09:14 -0400

    Validate photo type and size

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,12 +1,11 @@
 class Profile < ApplicationRecord
   belongs_to :user
-  has_one_attached :photo do |attachable|
-    attachable.variant :small, resize: "300x300"
-  end
+  has_one_attached :photo
 
   validates_presence_of :name
   validates_inclusion_of :grade, in: 9..12, message: "must be between 9 and 12"
   validate :must_like_some_gender
+  validate :acceptable_photo
 
   enum gender: %i[female male nonbinary]
 
@@ -19,7 +18,7 @@ class Profile < ApplicationRecord
       likes << "men"
     end
     if likes_nonbinary
-      likes << "nonbinary people"
+      likes << "non-binary people"
     end
     return likes.to_sentence
   end
@@ -28,6 +27,19 @@ class Profile < ApplicationRecord
     def must_like_some_gender
       unless likes_women || likes_men || likes_nonbinary  # if sexuality.blank
         errors.add(:base, "You must select at least one gender to match with")
+      end
+    end
+
+    def acceptable_photo
+      return unless photo.attached?
+
+      acceptable_types = ["image/jpeg", "image/jpg", "image/png"]
+      if not acceptable_types.include?(photo.content_type)
+        errors.add(:photo, "must be a JPEG or PNG")
+
+      elsif photo.byte_size > 2.megabytes
+        photo_size = (photo.byte_size / 1_000_000.0).round(1)
+        errors.add(:photo, "size (#{photo_size} MB) exceeds 2 MB limit")
       end
     end
 end
